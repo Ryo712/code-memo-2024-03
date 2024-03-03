a39dcf5ebb941f716b03486af04e54316fa4adc1
大城亮 <hiltoc.katsu@gmail.com>
2024-03-03 23:50:17 +0900

    serchフォーム

---
+++ 3.js
@@ -0,0 +1,10 @@
+//serchフォーム
+<div className="relative w-full mb-6">
+              <input
+                type="search"
+                className="w-full pl-1 pr-10 py-2 bg-gray-200 text-gray-700 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+                placeholder="Search"
+              />
+              <div className="absolute inset-y-0 right-0 inline-flex items-center pr-2">
+                <FontAwesomeIcon icon={faSearch} className="h-4 w-4 text-gray-500" />
+              </div>
